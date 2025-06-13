c641017bbb9292599b28312909f57f1483cf7ff1
William Gaston <z5591798@ad.unsw.edu.au>
2025-06-14 01:49:10 +1000

    finished primary encryption functions

---
+++ client/cryptography/encryption.py
@@ -0,0 +1,72 @@
+import os
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+def serialiseKeyPair(publicKey, privateKey, password):
+  privateSerialisedKey = privateKey.private_bytes(
+    encoding = serialization.Encoding.PEM,
+    format = serialization.PrivateFormat.PKCS8,
+    encryption_algorithm = serialization.BestAvailableEncryption(password.decode())
+  )
+
+  publicSerialisedKey = publicKey.public_bytes(
+    encoding = serialization.Encoding.PEM,
+    format = serialization.PublicFormat.SubjectPublicKeyInfo,
+  )
+
+  return privateSerialisedKey, publicSerialisedKey
+
+def deserializeKeyPair(publicSerialisedKey, privateSerialisedKey, password):
+  privateKey = serialization.load_pem_private_key(
+    privateSerialisedKey,
+    password=password.encode(),
+    backend=default_backend,
+  )
+
+  publicKey = serialization.load_pem_public_key(
+    publicSerialisedKey,
+    backend=default_backend,
+  )
+
+  return publicKey, privateKey
+
+def encryptMasterKey(masterKey, publicKey):
+  encryptedMasterKey = publicKey.encrypt(
+    masterKey,
+    padding.OAEP(
+      mgf=padding.MGF1(algorithm=hashes.SHA256()),
+      algorithm=hashes.SHA256(),
+      label=None
+    )
+  )
+
+  return encryptedMasterKey
+
+def decryptMasterKey(privateKey, encryptedMasterKey):
+  decryptedMasterKey = privateKey.decrypt(
+    encryptedMasterKey,
+    padding.OAEP(
+      mgf=padding.MGF1(algorithm=hashes.SHA256()),
+      algorithm=hashes.SHA256(),
+      label=None
+    )
+  )
+
+  return decryptedMasterKey
+
+def createCipherInstance(iv, masterKey):
+  return Cipher(algorithms.AES(masterKey), modes.CBC(iv))
+
+def encryptMessage(data, iv, masterKey):
+  cipherInstance = createCipherInstance(iv, masterKey)
+  encryptor = cipherInstance.encryptor()
+  cipherText = encryptor.update(data.encode()) + encryptor.finalize()
+  return cipherText
+  
+def decryptMessage(ciphertext, iv, masterKey):
+  cipherInstance = createCipherInstance(iv, masterKey)
+  decryptor = cipherInstance.decryptor()
+  rawText = decryptor.update(ciphertext) + decryptor.finalize()
+  return rawText
